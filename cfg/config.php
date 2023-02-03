862e4b4a2a1b8062ea14d129fcfc91eb3459dc45
williammitrefilho <william@DESKTOP-MO30ITB>
2023-02-02 22:04:20 -0300

    config

--- cfg/config.php
+++ cfg/config.php
@@ -1,14 +1,14 @@
 <?php
 define("DB_CHARSET", "utf8");
-define("DB_DBNAME", "meubonus");
+define("DB_DBNAME", "gerio");
 define("DB_HOST", "localhost");
-define("DB_PASSWORD", "");
+define("DB_PASSWORD", "Wmitre89895@");
 define("DB_PORT", 3306);
-define("DB_USER", "");
+define("DB_USER", "paripueira");
 
-define("DIR_CONTROLE", "/web/meubonus/classes/controle/");
-define("DIR_MODEL", "/web/meubonus/classes/model/");
-define("DIR_TEMPLATE", "/web/meubonus/templates/");
+define("DIR_CONTROLE", "/home/omg6agkxpq6j/public_html/meubonus/classes/controle/");
+define("DIR_MODEL", "/home/omg6agkxpq6j/public_html/meubonus/classes/model/");
+define("DIR_TEMPLATE", "/home/omg6agkxpq6j/public_html/meubonus/templates/");
 define("DIR_IMG", "meubonus/img/");
 define("DIR_SCRIPTS", "js/");
 define("URL_INDEX", "meubonus/");
