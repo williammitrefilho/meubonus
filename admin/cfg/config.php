32cd9f2ba76fdc726eb5e687b5356ec48022974e
williammitrefilho <william@DESKTOP-MO30ITB>
2023-02-06 20:50:23 -0300

    config file

--- admin/cfg/config.php
+++ admin/cfg/config.php
@@ -1,10 +1,10 @@
 <?php
 define("DB_CHARSET", "utf8");
-define("DB_DBNAME", "meubonus");
-define("DB_HOST", "locahost");
-define("DB_PASSWORD", "");
+define("DB_DBNAME", "gerio");
+define("DB_HOST", "localhost");
+define("DB_PASSWORD", "Wmitre89895@");
 define("DB_PORT", 3306);
-define("DB_USER", "");
+define("DB_USER", "paripueira");
 
 define("DIR_CONTROLE", "/home/omg6agkxpq6j/public_html/meubonus/admin/classes/controle/");
 define("DIR_MODEL", "/home/omg6agkxpq6j/public_html/meubonus/admin/classes/model/");
